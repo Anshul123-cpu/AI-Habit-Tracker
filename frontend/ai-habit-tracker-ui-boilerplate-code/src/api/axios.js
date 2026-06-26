0992ad231c85fe3331f1fff78575a9d03b899260
Anshul123-cpu <anshuldas000@gmail.com>
2026-06-26 23:23:11 +0530

    Fix production API URL

--- frontend/ai-habit-tracker-ui-boilerplate-code/src/api/axios.js
+++ frontend/ai-habit-tracker-ui-boilerplate-code/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+import server from "../environment.js";
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: `${server}/api`,
 });
 
 api.interceptors.request.use((config) => {
